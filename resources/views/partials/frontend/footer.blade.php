fbe7ce3d858e2d09ac961c0653231191d668c48d
Randika De Alwis <randika@motordat.com>
2026-01-23 12:15:49 +0530

    footer updated

--- resources/views/partials/frontend/footer.blade.php
+++ resources/views/partials/frontend/footer.blade.php
@@ -1,112 +1,66 @@
-<footer class="pt-15 pt-lg-20 pb-16 footer bg-section-4">
+<footer class="pt-15 pt-lg-20 pb-16 footer new-theme-primary-bg">
     <div class="container container-xxl pt-4">
         <div class="row">
-            <div class="col-lg col-md-4 col-12 mb-11 mb-lg-0 fs-14px">
-                <h3 class="fs-5 mb-6 ">Company</h3>
-
-
-                <p class="pe-xl-18 lh-2">
-                    Find a location nearest you. See <a class="" href="#"><u class="fw-medium">Our Stores</u></a>
-                </p>
-                <p class="mb-0 lh-2"><strong class="text-body-emphasis">+391 (0)35 2568 4593</strong>
-                    <br>
-                    hello@domain.com
-                </p>
+            <div class="col-sm-2 col-12 mb-11 mb-lg-0 fs-14px">
+                <img class="img-fluid" src="{{ asset('assets/common/images/logo.png') }}" alt="Siyota (Pvt) Ltd.">
+            </div>
+            <div class="col-sm-2 col-12 mb-11 mb-lg-0 fs-14px">
+            </div>
+            <div class="col-md-4 col-12 mb-11 mb-lg-0">
+                <h3 class="fs-5 mb-6 ">Connect with SIYOTA</h3>
+                <ul class="list-unstyled mb-0 fw-medium ">
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/') }}" class="text-body">Home</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/products') }}" class="text-body">Products</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/landscapes') }}" class="text-body">Landscapes</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/services') }}" class="text-body">Our Services</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/gallery') }}" class="text-body">Gallery</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/about-us') }}" class="text-body">About Us</a>
+                    </li>
+                    <li class="pt-3 mb-4">
+                        <a href="{{ url('/contact-us') }}" class="text-body">Contact Us</a>
+                    </li>
+                </ul>
 
             </div>
-            <div class="col-lg col-md-4 col-12 mb-11 mb-lg-0">
-                <h3 class="fs-5 mb-6 ">Useful links</h3>
+            <div class="col-md-4 col-12 mb-11 mb-lg-0">
+                <h3 class="fs-5 mb-6 ">Support</h3>
 
                 <ul class="list-unstyled mb-0 fw-medium ">
-
                     <li class="pt-3 mb-4">
-                        <a href="#" title="New Products" class="text-body">New Products</a>
+                        <a href="mailto:siyotaenterprises@gmail.com" class="text-body">siyotaenterprises@gmail.com</a>
                     </li>
-
                     <li class="pt-3 mb-4">
-                        <a href="#" title="Best Sellers" class="text-body">Best Sellers</a>
+                        <a href="tel:+94768781126" class="text-body">076 878 1126</a>
                     </li>
-
                     <li class="pt-3 mb-4">
-                        <a href="#" title="Bundle &amp; Save" class="text-body">Bundle &amp; Save</a>
-                    </li>
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Online Gift Card" class="text-body">Online Gift Card</a>
+                        <a href="javascript:void(0);" class="text-body">
+                            Siyota Enterprises
+                            <br>Panawenna
+                            <br>Pelmadulla
+                        </a>
                     </li>
 
                 </ul>
 
-            </div>
-            <div class="col-lg col-md-4 col-12 mb-11 mb-lg-0">
-                <h3 class="fs-5 mb-6 ">Information</h3>
-
-                <ul class="list-unstyled mb-0 fw-medium ">
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Start a Return" class="text-body">Start a Return</a>
-                    </li>
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Contact Us" class="text-body">Contact Us</a>
-                    </li>
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Shipping FAQ" class="text-body">Shipping FAQ</a>
-                    </li>
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Terms &amp; Conditions" class="text-body">Terms &amp; Conditions</a>
-                    </li>
-
-                    <li class="pt-3 mb-4">
-                        <a href="#" title="Privacy Policy" class="text-body">Privacy Policy</a>
-                    </li>
-
-                </ul>
-
-            </div>
-            <div class="col-lg-5 col-12 mb-11 mb-lg-0">
-                <h3 class="mb-4 ">Good emails.</h3>
-                <p class="lh-2 ">Enter your email below to be the first to know about new collections and product launches.</p>
-                <form class=" pt-8">
-                    <div class="input-group position-relative">
-                        <input type="email" class="form-control bg-body rounded-left" placeholder="Enter your email address">
-                        <button type="submit" class="btn btn-dark btn-hover-bg-primary btn-hover-border-primary ms-0">
-                            Subscribe
-                        </button>
-                    </div>
-                </form>
             </div>
         </div>
-        <div class="row align-items-center mt-0 mt-lg-20 pt-lg-4">
-            <div class="col-sm-12 col-md-6 col-lg-4 d-flex align-items-center order-2 order-lg-1 fs-6 mt-8 mt-lg-0">
-                <p class="mb-0">© Glowing 2023</p>
-                <ul class="list-inline fs-18px ms-6 mb-0">
-                    <li class="list-inline-item me-8">
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li class="list-inline-item me-8">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li class="list-inline-item me-8">
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                    </li>
-                </ul>
+        <div class="row align-items-center justify-content-between mt-0 mt-lg-20 pt-lg-4" style="border-top: 1px solid #E7E7E7;">
+            <div class="col-sm-12 col-md-6 col-lg-4 d-flex align-items-center order-2 order-lg-1 fs-6 mt-8">
+                <p class="mb-0">Copyright {{ date('Y', time()) }} © Siyota</p>
             </div>
-            <div class="col-sm-12 col-lg-4 text-md-center order-1 order-lg-2 ">
-                <a class="d-inline-block" href="index.html">
-
-
-                    <img class="lazy-image img-fluid light-mode-img" src="#" data-src="./assets/images/others/logo.png" width="179" height="26" alt="Glowing - Bootstrap 5 HTML Templates">
-                    <img class="lazy-image dark-mode-img img-fluid" src="#" data-src="./assets/images/others/logo-white.png" width="179" height="26" alt="Glowing - Bootstrap 5 HTML Templates">
-                </a>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 order-3 text-sm-start text-lg-end mt-8 mt-lg-0">
-                <img src="#" data-src="./assets/images/footer/img_1.png" width="313" height="28" alt="Paypal" class="img-fluid lazy-image">
+            <div class="col-sm-12 col-md-6 col-lg-4 order-3 text-sm-start text-lg-end mt-8">
+                <p class="mb-0">Designed & Developed by <a href="https://mediapassion.lk/" target="_blank">Media Passion</a></p>
             </div>
         </div>
 
