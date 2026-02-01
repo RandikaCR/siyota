134bc7fd0e75bcc8f30bb0bf9e68dc76246e144a
Randika De Alwis <randika@motordat.com>
2026-02-01 21:57:07 +0530

    favicon updated

--- resources/views/partials/frontend/head.blade.php
+++ resources/views/partials/frontend/head.blade.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>@yield('page_title') :: Siyota (Pvt) Ltd. Official Website</title>
-<link rel="icon" href="{{ asset('assets/frontend/images/others/favicon.ico') }}">
+<link rel="icon" href="{{ asset('assets/common/images/logo.png') }}">
 
 @php
     $metaTitle = !empty($metaTitle) ? $metaTitle : 'Siyota (Pvt) Ltd. Official Website';
