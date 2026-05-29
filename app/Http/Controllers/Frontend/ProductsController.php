88470ea58f801af64c294b74d799be525625524e
Randika De Alwis <randika@motordat.com>
2026-05-29 15:39:20 +0530

    fixed

--- app/Http/Controllers/Frontend/ProductsController.php
+++ app/Http/Controllers/Frontend/ProductsController.php
@@ -58,36 +58,6 @@ class ProductsController extends Controller
 
     public function view(Request $request, $slug = null){
 
-        $prices = ProductPrices::all();
-        $status = 1;
-        foreach ($prices as $price){
-            $productId = $price->product_id;
-
-            if (!empty($price->thickness_id)){
-                $tId = $price->thickness_id;
-                $pt = ProductThicknesses::where('product_id', $productId)->where('thickness_id', $tId)->first();
-                if (empty($pt)){
-                    $s = new ProductThicknesses();
-                    $s->product_id = $productId;
-                    $s->thickness_id = $tId;
-                    $s->status = $status;
-                    $s->save();
-                }
-            }
-
-            if (!empty($price->label_id)){
-                $lId = $price->label_id;
-                $pl = ProductLabels::where('product_id', $productId)->where('label_id', $lId)->first();
-                if (empty($pl)){
-                    $s = new ProductLabels();
-                    $s->product_id = $productId;
-                    $s->label_id = $lId;
-                    $s->status = $status;
-                    $s->save();
-                }
-            }
-        }
-        exit();
         $keyword = !empty($request->keyword) ? $request->keyword : null;
 
 
